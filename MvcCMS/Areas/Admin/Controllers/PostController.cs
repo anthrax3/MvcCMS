6acc621a1570763619880dfedfc3382132c670a5
ashafir <aviv@optimalfinance.co.il>
2015-01-30 23:25:52 +0200

    add post contoller code

---
+++ MvcCMS/Areas/Admin/Controllers/PostController.cs
@@ -0,0 +1,71 @@
+﻿using System.Web.Mvc;
+using MvcCMS.Models;
+
+namespace MvcCMS.Areas.Admin.Controllers
+{
+    [RouteArea("Admin")]
+    [RoutePrefix("post")]
+    public class PostController : Controller
+    {
+        // GET: Admin/Post
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //admin/post/create
+        [HttpGet]
+        [Route("create")]
+
+        public ActionResult Create()
+        {
+            //TODO: retrieve the model from data store
+            var model = new Post();
+
+            return View(model);
+        }
+
+        //admin/post/create
+        [HttpPost]
+        [Route("create")]
+
+        public ActionResult Create(Post model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            //TODO: create model in data store
+
+            return RedirectToAction("Index");
+        }
+
+
+        //admin/post/edit/post-to-edit
+        [HttpGet]
+        [Route("edit/{id}")]
+        public ActionResult Edit(string id)
+        {
+            //TODO: retrieve the model from data store
+            var model = new Post();
+
+            return View(model);
+        }
+
+        //admin/post/edit/post-to-edit
+        [HttpPost]
+        [Route("edit/{id}")]
+        public ActionResult Edit(Post model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            //TODO: update model in data store
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcCMS/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MvcCMS.Models
+{
+    public class Post
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? Published  { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
